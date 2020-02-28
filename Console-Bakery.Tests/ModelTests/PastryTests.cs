b2f304c04a2f8ad60b8a42ffb0d62b58dd2e8e18
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 14:45:44 -0800

    Test Pastry.CalculateOrder() method

--- Console-Bakery.Tests/ModelTests/PastryTests.cs
+++ Console-Bakery.Tests/ModelTests/PastryTests.cs
@@ -13,5 +13,13 @@ namespace ModelTests
       newPastry.AddPastries(3);
       Assert.AreEqual(3, newPastry.Quantity);
     }
+    [TestMethod]
+    public void CalculateOrder_CalculateTotalCostOf3Pastries_5()
+    {
+      Pastry newPastry = new Pastry();
+      newPastry.AddPastries(3);
+      newPastry.CalculateOrder();
+      Assert.AreEqual(5, newPastry.TotalCost);
+    }
   }
 }
