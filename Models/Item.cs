26b69be4d83ba47ed11f12cd27d45ae987374997
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 11:45:15 -0800

    Add ClearOrder method on Item class to be inheritted by Bread and Pastry

--- Models/Item.cs
+++ Models/Item.cs
@@ -5,5 +5,11 @@ namespace Models
     public int Cost { get; set; }
     public int Quantity { get; set; } = 0;
     public int TotalCost { get; set; } = 0;
+
+    public void ClearOrder()
+    {
+      Quantity = 0;
+      TotalCost = 0;
+    }
   }
 }
