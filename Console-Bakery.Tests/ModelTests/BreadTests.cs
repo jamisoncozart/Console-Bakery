7c1ce4fa43909933ffd649e777b6db04389520cd
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 15:11:48 -0800

    Update tests with updated methods

--- Console-Bakery.Tests/ModelTests/BreadTests.cs
+++ Console-Bakery.Tests/ModelTests/BreadTests.cs
@@ -7,17 +7,17 @@ namespace ModelTests
   public class BreadTests
   {
     [TestMethod]
-    public void AddBread_AddThreeToQuantity_3()
+    public void AddItems_AddThreeToQuantity_3()
     {
       Bread newBread = new Bread();
-      newBread.AddBread(3);
+      newBread.AddItems(3);
       Assert.AreEqual(3, newBread.Quantity);
     }
     [TestMethod]
     public void CalculateOrder_CalculateTotalCostOf3Breads_15()
     {
       Bread newBread = new Bread();
-      newBread.AddBread(3);
+      newBread.AddItems(3);
       newBread.CalculateOrder();
       Assert.AreEqual(15, newBread.TotalCost);
     }
--- Console-Bakery.Tests/ModelTests/PastryTests.cs
+++ Console-Bakery.Tests/ModelTests/PastryTests.cs
@@ -7,17 +7,17 @@ namespace ModelTests
   public class PastryTests
   {
     [TestMethod]
-    public void AddPastries_AddThreeToQuantity_3()
+    public void AddItems_AddThreeToQuantity_3()
     {
       Pastry newPastry = new Pastry();
-      newPastry.AddPastries(3);
+      newPastry.AddItems(3);
       Assert.AreEqual(3, newPastry.Quantity);
     }
     [TestMethod]
     public void CalculateOrder_CalculateTotalCostOf3Pastries_5()
     {
       Pastry newPastry = new Pastry();
-      newPastry.AddPastries(3);
+      newPastry.AddItems(3);
       newPastry.CalculateOrder();
       Assert.AreEqual(5, newPastry.TotalCost);
     }
