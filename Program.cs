1405f859a8ec87e6a656faa8c99034702f72c221
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 09:40:37 -0800

    Add PurchaseMenu function with loop to control for invalid input

--- Program.cs
+++ Program.cs
@@ -7,15 +7,19 @@ public class Program
   {
     Bread bread = new Bread();
     Pastry pastries = new Pastry();
+    MainMenu();
+  }
+  public static void MainMenu()
+  {
     bool invalidInput = true;
     while(invalidInput)
       {
-      string mainMenuChoice = MainMenu();
+      Console.WriteLine("Welcome to Console Bakery! Our current prices are: \nBread - $5 each (Buy 2 get 1 Free) | Pastry - $2 each (3 for $5) \nWould you like to: 'buy' or 'quit'");
+      string mainMenuChoice = Console.ReadLine();
       if(mainMenuChoice == "buy")
       {
         invalidInput = false;
-        //start purchase menu
-        Console.WriteLine($"Bread Cost: {bread.Cost}, Pastry Cost: {pastries.Cost}");
+        PurchaseMenu();
       }
       else if(mainMenuChoice == "quit")
       {
@@ -23,13 +27,36 @@ public class Program
       }
       else
       {
-        Console.WriteLine("That was not a valid command. Please type 'buy' or 'quit'");
+        Console.WriteLine("That was not a valid command.");
       }
     }
+    
   }
-  public static string MainMenu()
+  public static void PurchaseMenu()
   {
-    Console.WriteLine("Welcome to Console Bakery! Our current prices are: \nBread - $5 each (Buy 2 get 1 Free) | Pastry - $2 each (3 for $5) \nWould you like to: 'buy' or 'quit'");
-    return Console.ReadLine();
+    bool invalidInput = true;
+    while(invalidInput)
+    {
+      Console.WriteLine("What would you like to buy? \n'bread' or 'pastry' or 'quit'");
+      string purchaseMenuChoice = Console.ReadLine();
+      if(purchaseMenuChoice == "bread")
+      {
+        invalidInput = false;
+        //prompt number of bread
+      }
+      else if(purchaseMenuChoice == "pastry")
+      {
+        invalidInput = false;
+        //prompt number of pastries
+      }
+      else if(purchaseMenuChoice == "quit")
+      {
+        Environment.Exit(0);
+      }
+      else
+      {
+        Console.WriteLine("That was not a valid command.");
+      }
+    }
   }
 }
