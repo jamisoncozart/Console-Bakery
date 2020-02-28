6bfb2438c3f73d85bf6411a78702b249ec4b75b4
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 09:02:30 -0800

    Add Models directory with Bread and Pastry classes inheriting from Item class

---
+++ Models/Item.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+  public class Item
+  {
+    public static int Cost { get; set; }
+    public static int Quantity { get; set; } = 0;
+    public static int TotalCost { get; set; } = 0;
+  }
+}
