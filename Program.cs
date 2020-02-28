604c51351a6df7640129ab7d7b1c29ecdcdf047c
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 09:29:26 -0800

    Add Main Menu loop to control for invalid inputs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,31 @@ public class Program
 {
   public static void Main()
   {
-    Bread.Cost = 5;
-    Console.WriteLine($"Bread Cost: {Bread.Cost}, Quantity: {Bread.Quantity}, Total Cost: {Bread.TotalCost}");
+    Bread bread = new Bread();
+    Pastry pastries = new Pastry();
+    bool invalidInput = true;
+    while(invalidInput)
+      {
+      string mainMenuChoice = MainMenu();
+      if(mainMenuChoice == "buy")
+      {
+        invalidInput = false;
+        //start purchase menu
+        Console.WriteLine($"Bread Cost: {bread.Cost}, Pastry Cost: {pastries.Cost}");
+      }
+      else if(mainMenuChoice == "quit")
+      {
+        Environment.Exit(0);
+      }
+      else
+      {
+        Console.WriteLine("That was not a valid command. Please type 'buy' or 'quit'");
+      }
+    }
+  }
+  public static string MainMenu()
+  {
+    Console.WriteLine("Welcome to Console Bakery! Our current prices are: \nBread - $5 each (Buy 2 get 1 Free) | Pastry - $2 each (3 for $5) \nWould you like to: 'buy' or 'quit'");
+    return Console.ReadLine();
   }
 }
