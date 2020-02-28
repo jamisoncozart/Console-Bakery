482e2e39f35be3a520fb579a6280c525250200eb
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 11:47:12 -0800

    Add a CompleteOrder method to handle calling calculation methods and printing reciept

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Models;
 
 public class Program
@@ -14,6 +15,7 @@ public class Program
     bool invalidInput = true;
     while(invalidInput)
       {
+      Console.Clear();
       Console.WriteLine("Welcome to Console Bakery! Our current prices are: \nBread - $5 each (Buy 2 get 1 Free) | Pastry - $2 each (3 for $5) \nWould you like to: 'buy' or 'quit'");
       string mainMenuChoice = Console.ReadLine();
       if(mainMenuChoice == "buy")
@@ -28,6 +30,7 @@ public class Program
       else
       {
         Console.WriteLine("That was not a valid command.");
+        Thread.Sleep(2000);
       }
     }
   }
@@ -36,6 +39,7 @@ public class Program
     bool invalidInput = true;
     while(invalidInput)
     {
+      Console.Clear();
       Console.WriteLine("What would you like to buy? \n'bread' or 'pastry' or 'quit'");
       string purchaseMenuChoice = Console.ReadLine();
       if(purchaseMenuChoice == "bread")
@@ -55,13 +59,21 @@ public class Program
       else
       {
         Console.WriteLine("That was not a valid command.");
+        Thread.Sleep(2000);
       }
     }
   }
   public static void BuyItem(string item)
   {
-    Console.WriteLine($"Please enter the amount of {item} you would like to buy: ($5 each (Buy 2 get 1 Free)");
-    int quantityToPurchase = int.Parse(Console.ReadLine());
+    Console.Clear();
+    bool result = false;
+    int quantityToPurchase = 0;   
+    while(!result)
+    {
+      Console.WriteLine($"Please enter the amount of {item} you would like to buy?\n(Only purchase the amount of {item} you want to pay for. We will add the free {item}(s) when you complete your order.)");
+      string userString = Console.ReadLine();  
+      result = int.TryParse(userString, out quantityToPurchase);
+    }
     if(item == "pastry")
     {
       pastries.AddPastries(quantityToPurchase);
@@ -78,6 +90,7 @@ public class Program
     bool invalidInput = true;
     while(invalidInput)
     {
+      Console.Clear();
       Console.WriteLine("Would you like to:\n'complete order' or 'buy more'");
       string answer = Console.ReadLine();
       if(answer == "complete order")
@@ -93,6 +106,37 @@ public class Program
       else
       {
         Console.WriteLine("That was not a valid command.");
+        Thread.Sleep(2000);
+      }
+    }
+  }
+  public static void CompleteOrder()
+  {
+    bread.CalculateOrder();
+    pastries.CalculateOrder();
+    Console.Clear();
+    Console.WriteLine($"Your total is: ${bread.TotalCost + pastries.TotalCost}\nBread: {bread.Quantity}\nPastries: {pastries.Quantity}\nThank you for your purchase!");
+    bool invalidInput = true;
+    while(invalidInput)
+    {
+      Console.WriteLine("Would you like to:\n'restart' or 'exit'");
+      string restartAnswer = Console.ReadLine();
+      if(restartAnswer == "restart")
+      {
+        invalidInput = false;
+        bread.ClearOrder();
+        pastries.ClearOrder();
+        MainMenu();
+      }
+      else if(restartAnswer == "exit")
+      {
+        Environment.Exit(0);
+      }
+      else
+      {
+        Console.Clear();
+        Console.WriteLine("That was not a valid command.");
+        Thread.Sleep(2000);
       }
     }
   }
