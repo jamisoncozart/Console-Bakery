76dfafa4108c0f71672277780cc6353581215c22
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 14:39:03 -0800

    Test for Bread.CalculateOrder()

--- Console-Bakery.Tests/ModelTests/BreadTests.cs
+++ Console-Bakery.Tests/ModelTests/BreadTests.cs
@@ -7,11 +7,19 @@ namespace ModelTests
   public class BreadTests
   {
     [TestMethod]
-    public void AddBread_AddThreeToQuantity_Three()
+    public void AddBread_AddThreeToQuantity_3()
     {
       Bread newBread = new Bread();
       newBread.AddBread(3);
       Assert.AreEqual(3, newBread.Quantity);
+    }
+    [TestMethod]
+    public void CalculateOrder_CalculateTotalCostOf3Breads_15()
+    {
+      Bread newBread = new Bread();
+      newBread.AddBread(3);
+      newBread.CalculateOrder();
+      Assert.AreEqual(15, newBread.TotalCost);
     }
   }
 }
