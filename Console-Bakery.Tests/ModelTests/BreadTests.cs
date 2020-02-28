c98f686c685633c08df243a5d7b6fdb6e23619f3
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 14:24:56 -0800

    Restructure directory, Add testing environment

---
+++ Console-Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models;
+
+namespace ModelTests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void AddBread_AddThreeToQuantity_Three()
+    {
+      //tests here
+    }
+  }
+}
