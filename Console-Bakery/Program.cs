480ff0c88a31314c554c0a8f3b0571d08ba950e9
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-28 16:37:31 -0800

    Add text crawl to all Console.WriteLine statements

--- Console-Bakery/Program.cs
+++ Console-Bakery/Program.cs
@@ -16,7 +16,7 @@ public class Program
   public static void MainMenu()
   {
     Console.Clear();
-    ValidateInput("Welcome to Console Bakery! Our current prices are: \nBread - $5 each (Buy 2 get 1 Free) | Pastry - $2 each (3 for $5) \nWould you like to: 'buy' or 'quit'", "buy", PurchaseMenu);
+    ValidateInput("\nWelcome to Console Bakery! Our current prices are: \nBread - $5 each (Buy 2 get 1 Free) | Pastry - $2 each (3 for $5) \n\nWould you like to: 'buy' or 'quit'\n", "buy", PurchaseMenu);
   }
 
   public static void PurchaseMenu()
@@ -25,7 +25,7 @@ public class Program
     while(invalidInput)
     {
       Console.Clear();
-      Console.WriteLine("What would you like to buy? \n'bread' or 'pastry' or 'quit'");
+      RollingConsoleWrite("\nWhat would you like to buy? \n'bread' or 'pastry' or 'quit'\n");
       string purchaseMenuChoice = Console.ReadLine();
       if(purchaseMenuChoice == "bread")
       {
@@ -39,12 +39,13 @@ public class Program
       }
       else if(purchaseMenuChoice == "quit")
       {
+        RollingConsoleWrite("Goodbye!");
         Environment.Exit(0);
       }
       else
       {
-        Console.WriteLine("That was not a valid command!");
-        Thread.Sleep(2000);
+        RollingConsoleWrite("That was not a valid command!");
+        Thread.Sleep(1000);
       }
     }
   }
@@ -57,7 +58,7 @@ public class Program
     int quantityToPurchase = 0;   
     while(!result)
     {
-      Console.WriteLine($"Please enter the amount of {item} you would like to buy?\n(Only purchase the amount of {item} you want to pay for. We will add the free {item}(s) when you complete your order.)");
+      RollingConsoleWrite($"Please enter the amount of {item} you would like to buy?\n(Only purchase the amount of {item} you want to pay for. We will add the free {item}(s) when you complete your order.)\n");
       string userString = Console.ReadLine();  
       result = int.TryParse(userString, out quantityToPurchase);
     }
@@ -77,7 +78,7 @@ public class Program
   public static void BuyMore()
   {
     Console.Clear();
-    ValidateInput("Would you like to:\n'complete order' or 'buy more'", "complete order", CompleteOrder, "buy more", PurchaseMenu);
+    ValidateInput("Would you like to:\n'complete order' or 'buy more'\n", "complete order", CompleteOrder, "buy more", PurchaseMenu);
   }
 
   // Calculates and prints reciept
@@ -86,8 +87,8 @@ public class Program
     bread.CalculateOrder();
     pastries.CalculateOrder();
     Console.Clear();
-    Console.WriteLine($"Your total is: ${bread.TotalCost + pastries.TotalCost}\nBread: {bread.Quantity}\nPastries: {pastries.Quantity}\nThank you for your purchase!");
-    ValidateInput("Would you like to:\n'restart' or 'quit'", "restart", MainMenu);
+    RollingConsoleWrite($"Your total is: ${bread.TotalCost + pastries.TotalCost}\nBread: {bread.Quantity}\nPastries: {pastries.Quantity}\nThank you for your purchase!\n");
+    ValidateInput("Would you like to:\n'restart' or 'quit'\n", "restart", MainMenu);
   }
 
   // Runs loop to check for valid user input. Checks for passed in valid answers and runs passed in functions if user inputs valid answer
@@ -96,7 +97,7 @@ public class Program
     bool invalidInput = true;
     while(invalidInput)
       {
-      Console.WriteLine(prompt);
+      RollingConsoleWrite(prompt);
       string userChoice = Console.ReadLine();
       if(userChoice == option1)
       {
@@ -120,14 +121,23 @@ public class Program
       }
       else if(userChoice == "quit")
       {
+        RollingConsoleWrite("Goodbye!");
         Environment.Exit(0);
       }
       else
       {
-        Console.WriteLine("That was not a valid command!");
-        Thread.Sleep(2000);
+        RollingConsoleWrite("That was not a valid command!");
+        Thread.Sleep(1000);
         Console.Clear();
       }
+    }
+  }
+  public static void RollingConsoleWrite(string text)
+  {
+    for(int i = 0; i < text.Length; i++)
+    {
+      Console.Write(text[i]);
+      Thread.Sleep(40);
     }
   }
 }
